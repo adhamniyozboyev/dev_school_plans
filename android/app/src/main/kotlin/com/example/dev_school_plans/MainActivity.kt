be5f3b730aa759dac585f13304b7f455747e417b
adhamniyozboyev <adhamjonniyozboyev@gmail.com>
2023-04-27 00:50:07 -0700

    init

---
+++ android/app/src/main/kotlin/com/example/dev_school_plans/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.dev_school_plans
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+void main(){
+  runApp(
+    Container(
+      height: 10,
+      width: 10,
+      color: Colors.red,
+    ));
+}
