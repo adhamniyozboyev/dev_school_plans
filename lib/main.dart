325503a761b388d9af6f0959f5a17cd68f638f8f
adhamniyozboyev <adhamjonniyozboyev@gmail.com>
2023-04-29 09:20:03 +0500

    Regex

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,39 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
-void main(){
-  runApp(
-    Container(
-      height: 10,
-      width: 10,
-      color: Colors.red,
-    ));
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatefulWidget {
+  const MyApp({super.key});
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+          body: TextFormField(
+            inputFormatters: [
+              FilteringTextInputFormatter.allow(RegExp('[a-z]'))
+            ],
+        decoration: InputDecoration(
+            icon: Icon(Icons.person),
+            hintText: 'What is your name',
+            labelText: 'Name*'),
+        onSaved: (String? value) {
+          setState(() {});
+        },
+        validator: (String? value) {
+          return (value != null && value.contains('@')
+              ? 'Don\'t use a @ char.'
+              : null);
+        },
+      )),
+    );
+  }
 }
